33c32f002df21c37f1ae349eaeea7e17c4b16185
rayzeydmusee <dmusee721@gmail.com>
2022-08-29 21:49:12 +0300

    Mongo product id error

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -2,7 +2,13 @@ const ErrorHandler= require("../utils/errorhandler");
 
 module.exports=(err,req,res,next) =>{
     err.statusCode= err.statusCode || 500;
-    err.message= err.message || "Internal Server Error"
+    err.message= err.message || "Internal Server Error";
+
+    //wrong Mongo product id error
+    if (err.name ==="CastError"){
+        const message =`Resource not found. Invalid: ${err.path}`;
+        err=new ErrorHandler(message,400)
+    }
 
     res.status(err.statusCode).json({
         success:false,
