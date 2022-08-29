7af0339960104b333fca159d12ca1e7b1b5d0232
rayzeydmusee <dmusee721@gmail.com>
2022-08-29 21:42:40 +0300

    server uncaught exception,promise rejection

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const app=require("./app");
 const dotenv=require("dotenv");
 const connectDatabase = require("./config/database");
 
+//handling uncaught exception
+process.on("uncaughtException",(err)=>{
+  console.log(`Error: ${err.message}`);
+  console.log("shutting down the server due to uncaught exception");
+  process.exit(1)
+})
+
 //config
 
 dotenv.config({path:"backend/config/config.env"});
@@ -13,4 +20,15 @@ connectDatabase();
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
+  });
+
+   
+  //unhandles promise rejection
+  process.on("unhandledRejection", err=>{
+    console.log(`Error: ${err.message}`);
+    console.log("shutting down the server due to unhandles promise rejection");
+
+    server.close(()=>{
+      process.exit(1);
+    });
   });
