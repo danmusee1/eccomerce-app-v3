54a638b99954e6fd92a5f86134d00c320c9ff6df
rayzeydmusee <dmusee721@gmail.com>
2022-09-13 20:02:41 +0300

    Making Order API

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,16 @@ app.use(cookieParser())
 //Route imports
 const product= require("./routes/productRoute");
 const user= require("./routes/userRoute");
+const order=require("./routes/orderRoute");
+
+
 app.use("/api/v1",product);
 //route import-user
-app.use("/api/v1", user)
+app.use("/api/v1", user);
+app.use("/api/v1",order);
+
+
+
 //Midleware for erros
 app.use(errorMiddleware);
 module.exports=app;
